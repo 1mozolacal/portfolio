5497a11d71e342c1c4e3ef8e37a7754d76b4a513
Calvin Mozola <calvin@stuntfactory.com>
2020-09-16 11:41:55 -0400

    Basics of the home page

---
+++ portfolio-app/src/components/navBar.js
@@ -0,0 +1,18 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const NavBar = () => {
+  return (
+    <div class="topnav">
+      <a class="active" href="#home">
+        Home
+      </a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+  )
+}
+
+export default NavBar
